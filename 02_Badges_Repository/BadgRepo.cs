e46606075b0dc4444bc7e9c2d4ec2e18141e4016
dawita <dawit.fitru@gmail.com>
2020-12-13 14:02:54 -0500

    2nd project 2nd push

---
+++ 02_Badges_Repository/BadgRepo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Badges_Repository
+{
+    public class BadgRepo
+    {
+        private List<Badge> _badges = new List<Badge>();
+        private int badgeID = 1000;
+
+        // Badge create 
+
+        public void AddBageForTheDoor (Badge bage)
+        {
+            bage.BadgeID = badgeID + 1;
+            _badges.Add(bage);
+            badgeID++;
+        }
+        //Developer Read
+        public List<Badge> GetAllBadge()
+        {
+            return _badges;
+        }
+
+        //Helper Method
+        public Badge GetBadgeByID(int id)
+        {
+            foreach (Badge badge in _badges)
+            {
+                if (badge.BadgeID == id)
+                {
+                    return badge;
+                }
+
+            } return null;
+            
+        }
+        // badge Update
+
+        public bool BadgeUpdate(int badgeID, Badge newBadge)
+        {
+            Badge oldBadge = GetBadgeByID(badgeID);
+            if (oldBadge!=null)
+            {
+                oldBadge.BadgeID = newBadge.BadgeID;
+                oldBadge.DoorName = newBadge.DoorName;
+
+                return true;
+            }
+            return false;
+        }
+
+        //Remove Bage
+        public bool RemoveBage(int badgeid)
+        {
+            Badge badge = GetBadgeByID(badgeid);
+            if (_badges.Remove(badge))
+            {
+                return true;
+
+            }
+            return false;
+        }
+        
+    }
+}
---
+++ 02_Badges_Repository/Badge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Badges_Repository
+{
+    public class Badge
+    {
+        public int BadgeID { get; set; }
+        public string DoorName { get; set; }
+        private Dictionary<int, Badge> doorBadge = new Dictionary<int, Badge>();
+        public Badge() { }
+        public Badge(int id,string doorName,Dictionary<int,Badge> lsitOfBadge)
+        {
+            BadgeID = id;
+            DoorName = doorName;
+            doorBadge = lsitOfBadge;
+
+        }
+    }
+}
